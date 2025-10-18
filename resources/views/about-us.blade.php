ff973c27dea14330439f00ad742a54a2b63edfe7
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-10-18 07:14:42 +0000

    about-us page

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+  <x-navbar />
+
+  <h1 class="text-3xl text-center mt-10 mb-5 font-bold">About Us</h1>
+  <p class="mx-auto text-center text-lg max-w-[44rem]">
+    Distinctio laudantium necessitatibus id excepturi velit laboriosam. Et dolorum quis harum maiores nisi ex deserunt. Alias deleniti sit expedita consequatur deleniti. Dolorum quam quidem deleniti labore tenetur corrupti eum.
+  </p>
+
+  <div class="flex gap-10 my-20 max-w-[80rem] mx-auto">
+    <div>
+      <h2 class="font-semibold text-3xl mb-4">We are here to help kids <span class="text-green-500
+        font-bold">learn</span> and grow and achieve their dreams!</h2>
+      <p class="text-lg my-4">
+        Distinctio laudantium necessitatibus id excepturi velit laboriosam. Et dolorum quis harum maiores nisi ex deserunt. Alias deleniti sit expedita consequatur deleniti. Dolorum quam quidem deleniti labore tenetur corrupti eum.
+      </p>
+
+      <p class="text-lg">
+        Distinctio laudantium necessitatibus id excepturi velit laboriosam. Et dolorum quis harum maiores nisi ex deserunt. Alias deleniti sit expedita consequatur deleniti. Dolorum quam quidem deleniti labore tenetur corrupti eum.
+      </p>
+    </div>
+
+    <img class="max-w-[40rem] rounded-md" src="students.jpg" />
+  </div>
+
+  <x-footer />
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get("/", function () {
   return view("home");
 });
 
+Route::get("/about-us", function() {
+  return view("about-us");
+});
+
 Route::get("/profile", function() {
   return view("profile", ["user" => Auth::user()]);
 })->middleware("auth");
